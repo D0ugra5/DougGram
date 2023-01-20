bc7e2d81e5e91d64e04bbfa7dcd9d7629d220786
Douglas Sousa <douglasmb87@gmail.com>
2023-01-20 14:48:15 -0300

    chore add configure initial

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+
+      <title>DougGram</title>
+      <link rel="icon" type="image" href="favicon.png" />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
