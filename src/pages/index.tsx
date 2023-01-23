411aa5ba50198d818b22b9bb6bd9b516e586a19a
Douglas Sousa <douglasmb87@gmail.com>
2023-01-23 13:30:07 -0300

    feat: add layout tela login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,78 @@
+import {
+  Flex,
+  Image,
+  Box,
+  FormControl,
+  Input as ChakraInput,
+  Button,
+} from "@chakra-ui/react";
+
 export default function Home() {
-  return <h1>Clone Instagram </h1>;
+  return (
+    <Flex
+      w="100vw"
+      bg="dougGramColor.backGroundColor"
+      h="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Flex>
+        <Box boxSize="sm">
+          <Image src="backgroundPhone.png" alt="Mobile Phone" />
+        </Box>
+      </Flex>
+      <Flex
+        alignContent="center"
+        justifyContent="center"
+        flexDirection="column"
+        bg="dougGramColor.backGroundSecondary"
+        border="dougGramColor.borderPrimary"
+        borderWidth="2px"
+      >
+        <Flex alignContent="center" justifyContent="center" padding="2rem">
+          <Image
+            width="12rem"
+            height="4rem"
+            src="logoBlack.png"
+            alt="Letreiro"
+          />
+        </Flex>
+        <Flex padding="2rem">
+          <FormControl display="flex" flexDirection="column">
+            <ChakraInput
+              name="Douglas"
+              height="3rem"
+              width="19rem"
+              mb="1rem"
+              placeholder="Telefone, nome de usuário ou email"
+              focusBorderColor="dougGramColor.borderSelect"
+              borderColor="dougGramColor.borderSelectInput"
+            />
+            <ChakraInput
+              type="password"
+              height="3rem"
+              width="19rem"
+              placeholder="Senha"
+              focusBorderColor="dougGramColor.borderSelect"
+            />
+
+            <Button
+              color="dougGramColor.backGroundSecondary"
+              background="dougGramColor.colorBluePrimary"
+              mt="2rem"
+              width="19rem"
+              height="2.5rem"
+              variant="solid"
+              _hover={{
+                boxShadow:
+                  "0 0 1px 2px rgba(88, 144, 255, .75), 0 1px 1px rgba(0, 0, 0, .15)",
+              }}
+            >
+              Entrar
+            </Button>
+          </FormControl>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
 }
---
+++ styles/theme-global/theme.ts
@@ -0,0 +1,15 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    dougGramColor: {
+      backGroundColor: "##fafafa",
+      backGroundSecondary: "#ffffff",
+      colorBluePrimary: "#0095f6",
+      colorBlueSecondary: "#81cafb",
+      borderPrimary: "#dbdbdb",
+      borderFocusSelect: "#606770",
+      borderSelectInput: "#e4e4e4",
+    },
+  },
+});
